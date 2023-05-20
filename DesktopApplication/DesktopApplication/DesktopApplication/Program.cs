0173c0b3a9c82365f9e5796682f7f38b3818759a
daki4 <yordanmitev03@protonmail.com>
2023-05-20 18:52:19 +0200

    secrets support

--- DesktopApplication/DesktopApplication/DesktopApplication/Program.cs
+++ DesktopApplication/DesktopApplication/DesktopApplication/Program.cs
@@ -14,18 +14,13 @@ public static class Program
         // To customize application configuration such as set high DPI settings or default font,
         // see https://aka.ms/applicationconfiguration.
 
-        DotEnv.Load("../../../../../../.env");
+        DotEnv.Load("../../../.env");
         ApplicationConfiguration.Initialize();
         Debug.WriteLine(Environment.GetEnvironmentVariable("MQTT_URL"));
-        //IMqttProvider mqtt = new MqttProvider.MqttProvider(
-        //    Environment.GetEnvironmentVariable("MQTT_URL"),
-        //    Environment.GetEnvironmentVariable("MQTT_USER"),
-        //    Environment.GetEnvironmentVariable("MQTT_PASSWORD"));
         IMqttProvider mqtt = new MqttProvider(
-            "mqtt.yordanmitev.me",
-            "3dPrinterUsr",
-            "somepassword"
-            );
+            Environment.GetEnvironmentVariable("MQTT_URL")!.Trim(),
+            Environment.GetEnvironmentVariable("MQTT_USER")!.Trim(),
+            Environment.GetEnvironmentVariable("MQTT_PASSWORD")!.Trim());
         await mqtt.Subscribe("3dPrinter/1/#");
         await mqtt.Subscribe("3dprinter/1/#");
         await mqtt.Publish("test", "test2");
